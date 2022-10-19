92827ad19d1317563913267e3d54460e7bbbde8a
karthik30072001 <karthik3007201@gmail.com>
2022-10-19 13:05:36 +0530

    add three digit number and find average

---
+++ addThreeDigit.js
@@ -0,0 +1,10 @@
+//addThreeDigit.js
+//add three digit number and find average
+
+let a=Math.floor(Math.random()*900+100)
+let b=Math.floor(Math.random()*900+100)
+let c=Math.floor(Math.random()*900+100)
+let d=Math.floor(Math.random()*900+100)
+let e=Math.floor(Math.random()*900+100)
+console.log(a+b+c+d+e)
+console.log(Math.floor((a+b+c+d+e)/5))
