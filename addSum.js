f00d4aafcc38d557e16da54b09751dc3e71dcd5c
karthik30072001 <karthik3007201@gmail.com>
2022-10-19 13:42:24 +0530

    its ordinary

---
+++ addSum.js
@@ -0,0 +1,20 @@
+//ADd Three number
+let a=4 
+let b=7
+let c=9
+console.log(a+b+c)
+//Subtract  three numbers
+let a2=7
+let b2=4
+let c2= 2
+console.log(a2-b2-c2)
+//
+let a1=9
+let b1=12
+let c1 = 5
+console.log(a1*b1*c1)
+//Divide three numbers
+let a4= 400
+let b4 = 8
+let c4 = 5
+console.log(a4/b4/c4)
