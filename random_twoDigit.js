b49ecc7d56de22c9a25a409c2e9c6c53c121b8c6
karthik30072001 <karthik3007201@gmail.com>
2022-10-19 12:57:42 +0530

    Pick two random digit

---
+++ random_twoDigit.js
@@ -0,0 +1,7 @@
+//Pick random two digit number
+let a=Math.floor(Math.random()*90+10)
+let b=Math.floor(Math.random()*90+10)
+let c=Math.floor(Math.random()*90+10)
+let d=Math.floor(Math.random()*90+10)
+let e=Math.floor(Math.random()*90+10)
+console.log(a,b,c,d,e)
