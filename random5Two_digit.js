9f94626ccf3e15c28aa5825f498a1e4e9be91b6e
karthik30072001 <karthik3007201@gmail.com>
2022-10-19 13:02:25 +0530

    Add 5 two digit number and find those average

---
+++ random5Two_digit.js
@@ -0,0 +1,9 @@
+//Add 5 two digit number and find those average
+
+let a=Math.floor(Math.random()*90+10)
+let b=Math.floor(Math.random()*90+10)
+let c=Math.floor(Math.random()*90+10)
+let d=Math.floor(Math.random()*90+10)
+let e=Math.floor(Math.random()*90+10)
+console.log(a+b+c+d+e)
+console.log(Math.floor((a+b+c+d+e)/5))
